768df4a7ca82fe2a109fc0bf8742b6bde34b4397
Michael Bright <mjbright@github.com>
2015-09-21 16:27:07 +0200

    Modified print statements to be python3 compatible - print()

--- hide_code/hide_code.py
+++ hide_code/hide_code.py
@@ -8,7 +8,7 @@ def __main__():
 
 
 def install(nb_path=None, DEBUG=False):
-	print 'Starting hide_code.js install'
+	print('Starting hide_code.js install')
 	current_dir = path.abspath(path.dirname(__file__))
 	site_packages_path = os.__file__[:-6] + "site-packages"
 
@@ -18,22 +18,22 @@ def install(nb_path=None, DEBUG=False):
 
 	if nb_path != None:
 		install_path = np_path
-		print "Using " + install_path
+		print("Using " + install_path)
 
 	if DEBUG:
-		print install_path
+		print(install_path)
 
 	# copy js into static/custom directory in Jupyter/iPython directory
 	if(path.isdir(install_path)):
 		shutil.copyfile(current_dir + "/hide_code.js", install_path + "/hide_code.js")
-		print 'Copying hide_code.js to ' + install_path 
+		print('Copying hide_code.js to ' + install_path)
 
 		# add require to end of custom.js to auto-load on notebook startup
 		with open(install_path + "/custom.js", 'a') as customJS:
 			with open(current_dir + "/auto-load.txt") as auto:
 				customJS.write(auto.read())
-				print "Configured custom.js to auto-load hide_code.js"
+				print("Configured custom.js to auto-load hide_code.js")
 	else:
-		print 'Unable to install into ' + install_path
-		print 'Directory doesn\'t exist.'
-		print 'Make sure Jupyter is installed.'
+		print('Unable to install into ' + install_path)
+		print('Directory doesn\'t exist.')
+		print('Make sure Jupyter is installed.')
